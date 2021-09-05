3364c6a9475f287b6ddc41d43d3e5a894a4a0b12
Brian <Brian.reed1202@gmail.com>
2021-09-05 17:01:18 -0400

    Added more pseudocode and comments.

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -14,6 +14,11 @@ public class Solution04 {
         //Prompt user for an adverb
         //Prompt user for etc.
         //Print short story with String.format
+        /*Yesterday, (person) and I went to the park. On our way to the (adj) park,
+        we saw a (adj) (noun) on a bike. We also saw big (adj) balloons tied to a (noun).
+        Once we got to the (adj) park, the sky turned (adj). It started to (verb) (adverb).
+        (person) and I (past-tense verb) all the way home. Tomorrow we will try to go to the
+        (adj) park again and hope it doesn't (verb).*/
         String person1, adj1, adj2, adj3, adj4, noun1, noun2, verb1, verb2, adv1;
         System.out.print("Enter a person: ");
         person1 = scan.nextLine();
@@ -39,7 +44,7 @@ public class Solution04 {
                 "we saw a %s %s on a bike. \nWe also saw big %s balloons tied to a %s. \nOnce we got to the %s park, the sky turned %s. " +
                 "\nIt started to %s %s. \n%s and I %s all the way home. \nTomorrow we will try to go to the " +
                 "%s park again and hope it doesn't %s.", person1, adj1, adj2, noun1, adj3, noun2, adj1, adj4, verb1, adv1,
-                person1, verb2, adj1, verb1);
-        System.out.println(output);
+                person1, verb2, adj1, verb1);//Format statement
+        System.out.println(output);//Single output statement
     }
 }
