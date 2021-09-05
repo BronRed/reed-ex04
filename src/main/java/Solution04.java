7eeb533799646357464c45f3ae1c6c70268bb6f6
Brian <Brian.reed1202@gmail.com>
2021-09-05 17:28:38 -0400

    Changed output method.

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -40,11 +40,10 @@ public class Solution04 {
         adv1 = scan.nextLine();
         System.out.print("Enter a verb in the past-tense: ");
         verb2 = scan.nextLine();
-        String output = String.format("Yesterday, %s and I went to the park. \nOn our way to the %s park, " +
-                "we saw a %s %s on a bike. \nWe also saw big %s balloons tied to a %s. \nOnce we got to the %s park, the sky turned %s. " +
-                "\nIt started to %s %s. \n%s and I %s all the way home. \nTomorrow we will try to go to the " +
-                "%s park again and hope it doesn't %s.", person1, adj1, adj2, noun1, adj3, noun2, adj1, adj4, verb1, adv1,
-                person1, verb2, adj1, verb1);//Format statement
-        System.out.println(output);//Single output statement
+        System.out.print(String.format("Yesterday, %s and I went to the park. \nOn our way to the %s park, " +
+                        "we saw a %s %s on a bike. \nWe also saw big %s balloons tied to a %s. \nOnce we got to the %s park, the sky turned %s. " +
+                        "\nIt started to %s %s. \n%s and I %s all the way home. \nTomorrow we will try to go to the " +
+                        "%s park again and hope it doesn't %s.", person1, adj1, adj2, noun1, adj3, noun2, adj1, adj4, verb1, adv1,
+                person1, verb2, adj1, verb1));//Single output statement
     }
 }
